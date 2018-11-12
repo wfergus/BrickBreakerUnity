375c1d6c3b16b67c2ba2b1a205408a9dcb562c56
William Fergus <Will.Fergus@iam.local>
2018-11-12 15:09:44 -0600

    adds some script fixes

---
+++ Brick_Breaker_Unity/Assets/Scripts/BlockManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class BlockManager : MonoBehaviour
+{
+
+    public GameObject prefab;
+
+    private List<GameObject> Bricks;
+    private List<GameObject> bricksToRemove;
+    void Start()
+    {
+        this.LoadLevel();
+    }
+
+
+    public float gridX = 5f;
+    public float gridY = 5f;
+    public float padding = .05f;
+
+    public float startX = -1; //unity units startX
+    public float starty = 1; //unity units startY
+    public virtual void LoadLevel()
+    {
+        Bricks = new List<GameObject>();
+        bricksToRemove = new List<GameObject>();
+        for (float y = starty; y < gridY; y = y + padding)
+        {
+            for (float x = startX; x < gridX; x = x + padding)
+            {
+                Vector3 pos = new Vector3(x + padding, y + padding, 0);
+                var brick = (GameObject)Instantiate(prefab, pos, Quaternion.identity, this.transform);
+                brick.transform.parent = this.transform;
+                brick.name = brick.name + string.Format("{0}:{1}", x, y); //could also count blocks
+                Bricks.Add(brick);
+            }
+        }
+    }
+
+    //Win
+    void Win()
+    {
+        throw new System.Exception("Winner Winner Chicken Dinner!"); //bad way to win
+    }
+
+    void Update()
+    {
+        bricksToRemove.Clear();
+        foreach (var item in Bricks)
+        {
+            if (item == null)
+            {
+                bricksToRemove.Add(item);
+            }
+        }
+
+        foreach (var item in bricksToRemove)
+        {
+            Bricks.Remove(item);
+            //ScoreManager.Score += 1;
+        }
+
+        if (Bricks.Count == 0)
+        {
+            this.Win();
+        }
+    }
+}
---
+++ Brick_Breaker_Unity/Assets/Scripts/GameConsole.cs
@@ -0,0 +1,123 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public interface IGameConsole
+{
+    string FontName { get; set; }
+    string DebugText { get; set; }
+
+    string GetGameConsoleText();
+    void GameConsoleWrite(string s);
+}
+
+public class GameConsole : MonoBehaviour, IGameConsole
+{
+
+    protected string fontName;
+    public string FontName { get { return fontName; } set { fontName = value; } }
+
+    protected string debugText;
+    public string DebugText { get { return debugText; } set { debugText = value; } }
+
+    protected int maxLines;
+    public int MaxLines { get { return maxLines; } set { maxLines = value; } }
+
+    protected List<string> gameConsoleText;
+    protected GameConsoleState gameConsoleState;
+
+    public KeyCode ToggleConsoleKey;
+
+    public Text Console, Debug;
+
+    public void Awake()
+    {
+        ToggleConsoleKey = KeyCode.BackQuote;
+        gameConsoleText = new List<string>();
+        gameConsoleState = GameConsoleState.Open;
+        //ToggleConsole();
+        MaxLines = 15;
+        GameConsoleWrite("Awake Called");
+        debugText = "Default Debug Text";
+
+        //Test for Canvas and GameConsoleText and Debug Text
+    }
+
+    public void Update()
+    {
+        //Console.enabled = false;
+        if (gameConsoleState == GameConsoleState.Open)
+        {
+            Console.enabled = true;
+            Console.text = GetGameConsoleText();
+            Debug.text = DebugText;
+        }
+        else
+        {
+            Debug.text = string.Empty;
+            Console.text = string.Empty;
+
+        }
+        if (Input.GetKeyUp(ToggleConsoleKey))
+        {
+            this.ToggleConsole();
+        }
+
+    }
+
+    public void ToggleConsole()
+    {
+        if (this.gameConsoleState == GameConsoleState.Closed)
+        {
+            this.gameConsoleState = GameConsoleState.Open;
+            Console.enabled = true;
+            Debug.enabled = true;
+
+        }
+        else
+        {
+            this.gameConsoleState = GameConsoleState.Closed;
+            Console.enabled = false;
+            Debug.enabled = false;
+        }
+    }
+
+    public void Draw()
+    {
+
+    }
+
+    public string GetGameConsoleText()
+    {
+        string Text = "";
+
+        string[] current = new string[System.Math.Min(gameConsoleText.Count, MaxLines)];
+        int offsetLines = (gameConsoleText.Count / maxLines) * maxLines;
+
+        int offest = gameConsoleText.Count - offsetLines;
+
+        int indexStart = offsetLines - (maxLines - offest);
+        if (indexStart < 0)
+            indexStart = 0;
+
+        gameConsoleText.CopyTo(
+            indexStart, current, 0, System.Math.Min(gameConsoleText.Count, MaxLines));
+
+        foreach (string s in current)
+        {
+            Text += s;
+            Text += "\n";
+        }
+        return Text;
+    }
+
+    public void GameConsoleWrite(string s)
+    {
+        gameConsoleText.Add(s);
+    }
+
+    //Console State
+    public enum GameConsoleState { Closed, Open };
+}
